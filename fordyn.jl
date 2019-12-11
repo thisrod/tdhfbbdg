0d1ce1219f2fffd6c85da0feadfcde9047f8ea7b
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2019-12-11 17:41:34 +1100

    complete fordyn.jl

--- fordyn.jl
+++ fordyn.jl
@@ -6,14 +6,12 @@ using Plots, ComplexPhasePortrait, Printf
 C = 10;  μ = 15;  Ω = 2*0
 # h = 0.2;  N = 50
 h = 0.2;  N = 30
-a = 1.4;  b = 0.2		# SOR and thermal polations
+a = 1.4		# SOR polation
 
-y = h/2*(1-N:2:N-1);  x = y';  z = x .+ 1im*y
+y = h/2*(1-N:2:N-1);  x = y';  z = Complex.(x,y)
 V = r² = abs2.(z)
-k₀ = Tuple(keys(r²)[argmin(abs.(z.-0.5))])
+k₀ = Tuple(keys(r²)[argmin(abs.(z.-0.9))])
 ψ = Complex.(exp.(-r²/2)/√π);  ψ = conj.(z).*ψ./sqrt(1 .+ r²)
-# jitter to include L ≠ 0 component
-ψ += (0.1*randn(N,N) + 0.1im*randn(N,N)).*abs.(ψ)
 
 # Finite difference matrices.  ∂ on left is ∂y, ∂' on right is ∂x
 
@@ -25,9 +23,6 @@ end
 ∂ = (1/h).*op([-1/60, 3/20, -3/4, 0, 3/4, -3/20, 1/60])
 ∂² = (1/h^2).*op([1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])
 
-# find ground state solve by SOR: 
-# -∂²*ψ-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ)) = μ*ψ
-
 function impose_vortex!(ψ)
     # boundary condition ψ(z[k₀]) = 0, ψ is analytic at z[k₀]
     udlr = [(1,0),(0,1),(-1,0),(0,-1)]
@@ -38,56 +33,56 @@ function impose_vortex!(ψ)
     end
 end
 
-# components of BdG matrix
-
-eye = Matrix(I,N,N)
-H = -kron(eye, ∂²) - kron(∂², eye) + diagm(0=>V[:]) - μ*Matrix(I,N^2,N^2)
-J = 1im*(repeat(y,1,N)[:].*kron(∂,eye)-repeat(x,N,1)[:].*kron(eye,∂))
-
-    # solve by SOR: 
-    # -∂²*ψ-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ)) = μ*ψ
-
-Umd(i) = reshape(ev[1:N^2,i], N, N)
-Vmd(i) = reshape(ev[N^2+1:end,i], N, N)
+# solve by SOR: 
+# -∂²*ψ-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ)) = μ*ψ
 
 ψ₀ = similar(ψ)
-nnc = zero(ψ)
-    potential = []
-    residual = []
-    for _ = 1:Int(1000)
-        ψ₀ .= ψ
-        for k = keys(ψ)
-            i,j = Tuple(k)
-            ψ[k] = 0
-            T = ∂²[i:i,:]*ψ[:,j:j]+ψ[i:i,:]*∂²[:,j:j]
-            L = y[i]*(ψ[i:i,:]*∂'[:,j:j])-x[j]*(∂[i:i,:]*ψ[:,j:j])
-            ψk = (μ*ψ₀[k]+T[]+1im*Ω*L[]) /
-                (-2*∂²[1,1]+V[k]+C*(abs2.(ψ₀[k])+2nnc[k]))
-            ψ[k] = ψ₀[k] + a*(ψk-ψ₀[k])
-            impose_vortex!(ψ)
-         end
-         Lψ = -∂²*ψ-ψ*∂²+V.*ψ+C*(abs2.(ψ₀)+2nnc).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))
-         # take residual of unconstrained components
-         impose_vortex!(Lψ)
-         E = sum(conj.(ψ).*Lψ)/norm(ψ)^2 |> real
-         push!(potential, E)
-         push!(residual, norm(Lψ-E*ψ)/norm(ψ))
-    end
+nnc = zero(ψ)	# left in for future use
+potential = []
+residual = []
+@time for _ = 1:Int(1000)
+    ψ₀ .= ψ
+    for k = keys(ψ)
+        i,j = Tuple(k)
+        ψ[k] = 0
+        T = ∂²[i:i,:]*ψ[:,j:j]+ψ[i:i,:]*∂²[:,j:j]
+        L = y[i]*(ψ[i:i,:]*∂'[:,j:j])-x[j]*(∂[i:i,:]*ψ[:,j:j])
+        ψk = (μ*ψ₀[k]+T[]+1im*Ω*L[]) /
+            (-2*∂²[1,1]+V[k]+C*(abs2.(ψ₀[k])+2nnc[k]))
+        ψ[k] = ψ₀[k] + a*(ψk-ψ₀[k])
+        impose_vortex!(ψ)
+     end
+     Lψ = -∂²*ψ-ψ*∂²+V.*ψ+C*(abs2.(ψ₀)+2nnc).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))
+     # take residual of unconstrained components
+     impose_vortex!(Lψ)
+     E = sum(conj.(ψ).*Lψ)/norm(ψ)^2 |> real
+     push!(potential, E)
+     push!(residual, norm(Lψ-E*ψ)/norm(ψ))
+end
 
 # propagate
 
-P = ODEProblem((ψ,p,t)->-1im*(-∂²*ψ-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))), ψ, (0.0,1.0))
-S = solve(P)
-ψ = S.u[end]
+P = ODEProblem((ψ,p,t)->-1im*(-∂²*ψ-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))), ψ, (0.0,10.0))
+@time S = solve(P; saveat=0.5)
 
-    # sound wave spectrum
+evs = [];  ews = []
+
+for ψ in S.u
     
+    # BdG matrix
+    
+    eye = Matrix(I,N,N)
+    J = 1im*(repeat(y,1,N)[:].*kron(∂,eye)-repeat(x,N,1)[:].*kron(eye,∂))
+    H = -kron(eye, ∂²) - kron(∂², eye) + diagm(0=>V[:]) - μ*Matrix(I,N^2,N^2)
     Q = diagm(0=>ψ[:]);  R = 2C*(abs2.(Q)+diagm(0=>nnc[:]))
     BdGmat = [
         H+R-Ω*J    C*Q.^2;
         -C*conj.(Q).^2    -H-R-Ω*J
     ]
-    s = eigen(BdGmat)
+    
+    # find sound wave spectrum
+    
+    @time s = eigen(BdGmat)
     ev = s.vectors
     # This way seems to beat the rounding
     unms = h^2*sum(abs2.(ev[1:N^2,:]), dims=1)
@@ -95,20 +90,21 @@ S = solve(P)
     nsq = unms .- vnms
     ixs = sortperm(nsq[:], by=abs)
     ixs = ixs[1:10]
-    ew = s.values[ixs];  ev = ev[:,ixs];  nsq = nsq[:,ixs];  unms = unms[:,ixs];  vnms = vnms[:,ixs]
-    # σ = sortperm(ew)
-    # ew = ew[σ];  ev = ev[:,σ];  nsq = nsq[:,σ]
+    ew = s.values[ixs];  ev = ev[:,ixs];  nsq = nsq[:,ixs]
+    nsq[nsq .< 0.01] .= 1
     ev = ev./sqrt.(abs.(nsq))
-ωs = ew
+    push!(evs,ev)
+    push!(ews,ew)
+end
 
-L = (ψ[:]'*J*ψ[:])/norm(ψ)^2
+# L = (ψ[:]'*J*ψ[:])/norm(ψ)^2
 
-Jev = collect(real(ev[:,i]'*[J zero(J);  zero(J) J]*ev[:,i]-L/h^2)/norm(ev[:,i])^2 for i = 1:length(ωs))
+# Jev = collect(real(ev[:,i]'*[J zero(J);  zero(J) J]*ev[:,i]-L/h^2)/norm(ev[:,i])^2 for i = 1:length(ωs))
 
 Umd(i) = reshape(ev[1:N^2,i], N, N)
 Vmd(i) = reshape(ev[N^2+1:end,i], N, N)
 
-nsq = [h^2*sum(abs2.(Umd(i))-abs2.(Vmd(i))) for i = eachindex(ωs)]
+# nsq = [h^2*sum(abs2.(Umd(i))-abs2.(Vmd(i))) for i = eachindex(ωs)]
 
 zplot(ψ) = plot(y, y, portrait(reverse(ψ,dims=1)).*abs2.(ψ)/maximum(abs2.(ψ)), aspect_ratio=1)
 zplot(ψ::Matrix{<:Real}) = zplot(Complex.(ψ))
