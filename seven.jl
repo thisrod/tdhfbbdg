38897af523246f42d0225280b3e5b48f4fbb261a
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2020-01-22 16:25:20 +1100

    Switch to Optim.jl for seven vortex lattice

--- seven.jl
+++ seven.jl
@@ -1,11 +1,13 @@
 # Order parameter for a Bose-Einstein condensate with a lattice of
 # seven vortices
 
-using LinearAlgebra, BandedMatrices, JLD2
+using LinearAlgebra, BandedMatrices, Optim
 
-C = 10;  μ=25;  Ω=2*0.55
+g = 10;  Ω=2*0.55
+Nc = 116.24
+
 h = 0.2;  N = 100
-a = 1.7
+C = g*Nc/h^2		# Optim sets norm(ψ) = 1
 
 y = h/2*(1-N:2:N-1);  x = y';  z = x .+ 1im*y
 V = r² = abs2.(z)
@@ -13,6 +15,7 @@ V = r² = abs2.(z)
 ψ = z.^7 .*ψ./sqrt(1 .+ r²).^7
 # jitter to include L ≠ 7 components
 ψ += (0.1*randn(N,N) + 0.1im*randn(N,N)).*abs.(ψ)
+ψ ./= norm(ψ)
 
 # Finite difference matrices.  ∂ on left is ∂y, ∂' on right is ∂x
 
@@ -24,24 +27,22 @@ end
 ∂ = (1/h).*op([-1/60, 3/20, -3/4, 0, 3/4, -3/20, 1/60])
 ∂² = (1/h^2).*op([1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])
 
-# solve by SOR: 
-# -∂²*ψ-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ)) = μ*ψ
+# Minimise the energy 
+#
+# E(ψ) = -∫ψ*∇²ψ + V|ψ|²+g/2·|ψ|⁴-Ω·ψ*Jψ
+#
+# The GPE functional L(ψ) is the gradient required by Optim.
 
-residual = []
-ψ₀ = similar(ψ)
-for _ = 1:1
-    ψ₀ .= ψ
-    for k = keys(ψ)
-        i,j = Tuple(k)
-        ψ[k] = 0
-        T = ∂²[i:i,:]*ψ[:,j:j]+ψ[i:i,:]*∂²[:,j:j]
-        L = y[i]*(ψ[i:i,:]*∂'[:,j:j])-x[j]*(∂[i:i,:]*ψ[:,j:j])
-        ψk = (μ*ψ₀[k]+T[]+1im*Ω*L[]) /
-            (-2*∂²[1,1]+V[k]+C*(abs2.(ψ₀[k])))
-        ψ[k] = ψ₀[k] + a*(ψk-ψ₀[k])
-     end
-     Lψ = -∂²*ψ-ψ*∂²+V.*ψ+C*(abs2.(ψ₀)).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))
-     m = sum(conj.(ψ).*Lψ)/norm(ψ)^2
-     push!(residual, norm(Lψ-m*ψ)/norm(ψ))
+L(ψ) = -(∂²*ψ+ψ*∂²)+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))
+H(ψ) = -(∂²*ψ+ψ*∂²)+V.*ψ+C/2*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))
+E(ψ) = sum(conj.(ψ).*H(ψ)) |> real
+
+grdt!(buf,ψ) = copyto!(buf, L(ψ))
+
+# result = optimize(E, grdt!, ψ, ConjugateGradient(manifold=Sphere()));
+# ψ₀ = result.minimizer;
+
+function rdl(ψ)
+    μ = sum(conj.(ψ).*L(ψ)) |> real
+    norm(L(ψ)/μ-ψ)
 end
-
