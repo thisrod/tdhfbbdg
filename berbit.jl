87b4900b1d64bb580cf4e6506712cf606e31c56d
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2020-02-11 15:12:58 +1100

    Find a smooth offset vortex
    For some reason, the ConjugateGradient solver in Optim.jl generates
    noise at the grid edges.  GradientDescent is slower, but gives a
    smooth solution.
    
    The solvers are more efficient when preconditioned with the potential
    energy.
    
    See https://github.com/JuliaNLSolvers/Optim.jl/pull/774
    for details.

--- berbit.jl
+++ berbit.jl
@@ -1,18 +1,16 @@
 # Berry phase for an orbiting order parameter
 
-using LinearAlgebra, BandedMatrices, JLD2, DifferentialEquations
-using Plots, ComplexPhasePortrait, Printf
+using LinearAlgebra, BandedMatrices, Optim, JLD2
 
-C = 10;  μ=30;  Ω=2*0.15
-r₀ = 0.5		# offset of imprinted phase
-# C = 10;  μ=30;  Ω=2*0.13
-# r₀ = 0.2		# offset of imprinted phase
-h = 0.3;  N = 36
-a = 1.4		# SOR polation
+g = 10/sqrt(2);  Ω=0.15*sqrt(2)
+r₀ = (1.86279296875+1.86328125)/2		# offset of imprinted phase, bisection search
+Nc = 140.2749
+
+h = 0.15/2^(1/4);  N = 200
+C = g*Nc/h^2
 
 y = h/2*(1-N:2:N-1);  x = y';  z = x .+ 1im*y
 V = r² = abs2.(z)
-ψ = Complex.(exp.(-r²/2)/√π);  ψ = (z.-r₀).*ψ
 
 # Finite difference matrices.  ∂ on left is ∂y, ∂' on right is ∂x
 
@@ -24,77 +22,29 @@ end
 ∂ = (1/h).*op([-1/60, 3/20, -3/4, 0, 3/4, -3/20, 1/60])
 ∂² = (1/h^2).*op([1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])
 
-# components of BdG matrix
-
-eye = Matrix(I,N,N)
-H = -kron(eye, ∂²) - kron(∂², eye) + diagm(0=>V[:]) - μ*Matrix(I,N^2,N^2)
-J = 1im*(repeat(y,1,N)[:].*kron(∂,eye)-repeat(x,N,1)[:].*kron(eye,∂))
-
 # iterate to self-consistent thermal cloud
 
-nnc = zero(ψ)
-ψ₀ = similar(ψ)
-therm = []
-oprm = []
-rsdls = []
-μs = []
+ψ = z.-r₀
 
-memoized = false
-memfile = "ofst.jld2"
-if isfile(memfile)
-    @load memfile ψ prms
-    memoized =
-        size(ψ) == (N,N) &&
-        prms == Dict(:C=>C, :μ => μ, :Ω=>Ω, :h=>h)
-end
+# Minimise the energy 
+#
+# E(ψ) = -∫ψ*∇²ψ/2 + V|ψ|²+g/2·|ψ|⁴-Ω·ψ*Jψ
+#
+# The GPE functional L(ψ) is the gradient required by Optim.
 
-if memoized
-    println("Order parameter: here's one I prepared earlier")
-else
+L(ψ) = -(∂²*ψ+ψ*∂²)/2+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))
+Ham(ψ) = -(∂²*ψ+ψ*∂²)/2+V.*ψ+C/2*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))
+E(xy) = sum(conj.(togrid(xy)).*Ham(togrid(xy))) |> real
+grdt!(buf,xy) = copyto!(buf, 2*L(togrid(xy))[:])
+togrid(xy) = reshape(xy, size(z))
 
-    # solve by SOR: 
-    # -∂²*ψ-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ)) = μ*ψ
-    
-    rsdl = []
-    for n = 0:5000
-        ψ₀ .= ψ
-        for k = keys(ψ)
-            i,j = Tuple(k)
-            ψ[k] = 0
-            T = ∂²[i:i,:]*ψ[:,j:j]+ψ[i:i,:]*∂²[:,j:j]
-            L = y[i]*(ψ[i:i,:]*∂'[:,j:j])-x[j]*(∂[i:i,:]*ψ[:,j:j])
-            ψk = (μ*ψ₀[k]+T[]+1im*Ω*L[]) /
-                (-2*∂²[1,1]+V[k]+C*(abs2.(ψ₀[k])+2nnc[k]))
-            ψ[k] = ψ₀[k] + a*(ψk-ψ₀[k])
-         end
-         Lψ = -∂²*ψ-ψ*∂²+V.*ψ+C*(abs2.(ψ)+2nnc).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))
-         m = sum(conj.(ψ).*Lψ)/norm(ψ)^2
-         push!(μs, m)
-         push!(rsdl, norm(Lψ-m*ψ)/norm(ψ))
-         n % 50 == 0 && push!(oprm, copy(ψ₀))
-    end
-    
-    prms = Dict(:C=>C, :μ => μ, :Ω=>Ω, :h=>h)
-    @save memfile ψ prms
-end
-   
-# Rotate the order parameter
-P = ODEProblem((ψ,_,_)->-(y.*(ψ*∂')-x.*(∂*ψ)), ψ, (0.0,2π))
-S = solve(P)
+rc = 5	# condensate radius, 
+P = Diagonal(sqrt.(rc^4 .+ V[:].^2))
 
-Nop = h^2*norm(ψ.*(r² .< r₀^2))^2
-Nb(ε) = h^2/ε*imag(S(ε) ⋅S(0))
+result = optimize(E, grdt!, ψ[:],
+    GradientDescent(manifold=Sphere(), P=P),
+    Optim.Options(iterations = 10_000)
+)
+ψ = togrid(result.minimizer)
 
-# numerical Berry phase
-
-∫(u) = h^2*sum(u)
-ndiff(u,θ₀,θ₁) = imag.(conj.(u(θ₁)).*u(θ₀))
-
-function ∮(u, difun, m)
-    h = 2π/m
-    s = zero(z)
-    for j = 1:m
-       s .+= difun(u, (j-1)*h, j*h)
-    end
-    s
-end
+@save "orb.jld2" ψ
